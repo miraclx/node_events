481b38a8e8f51b45859d8fc8c2c91bf616472507
miraclx <omiraculous@gmail.com>
2019-05-22 01:50:14 +0100

    Setup as PyPI library

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="node_events",
+    version="0.5.0",
+    author="Miraculous Owonubi",
+    author_email="omiraculous@gmail.com",
+    description="A minor rewrite of the NodeJS EventEmitter in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache-2.0',
+    url="https://github.com/miraclx/node_events.py",
+    packages=['node_events'],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
