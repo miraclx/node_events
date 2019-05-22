4015df28bf46f75354dba66bf9666b5ea965de0e
miraclx <omiraculous@gmail.com>
2019-05-22 04:25:59 +0100

    Bumped to v0.6.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="node_events",
-    version="0.5.2",
+    version="0.6.0",
     author="Miraculous Owonubi",
     author_email="omiraculous@gmail.com",
     description="A minor rewrite of the NodeJS EventEmitter in Python",
