96af020010a459b7501c904393f2ba2584f36bd5
Miraclx <omiraculous@gmail.com>
2019-07-04 23:25:00 +0100

    Bumped patch version v0.6.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="node_events",
-    version="0.6.7",
+    version="0.6.8",
     author="Miraculous Owonubi",
     author_email="omiraculous@gmail.com",
     description="A minor rewrite of the NodeJS EventEmitter in Python",
